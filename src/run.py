a2306351aafaea3819094370467b854f21c1c4e0
Alexandra <salexa92@gmail.com>
2017-04-19 16:20:18 +0300

    Fixed bug

--- src/run.py
+++ src/run.py
@@ -41,7 +41,7 @@ def runForType(familyType):
 	for x in range(len(taxa)):
 		if familyType[0] in taxa[x]:
 			print('--- Running for {}. ---'.format(taxa[x]))
-			runAlgorithm([taxa[x]])
+			runAlgorithm(taxa[x])
 
 	# Record time passed.
 	print('*** Total runtime for family type {} was  {} ***'.format(familyType, datetime.now() - start))
